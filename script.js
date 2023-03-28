814622380ac0b2d62b414091ee380f369693a3e7
unknown <hjds760781@gmial.com>
2023-03-28 19:12:59 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 let newImg, showImg;
 
-function loadImg(event){
-  showImg=document.getElementById('showImg')
-  showImg.src=URL.createObjectURL(event.target.files[0]);
+function loadImg(event) {
+  showImg = document.getElementById("showImg");
+  showImg.src = URL.createObjectURL(event.target.files[0]);
 
-  newImg=document.createElement('img')
-  newImg.src=URL.createObjectURL(event.target.files[0]);
+  newImg = document.createElement("img");
+  newImg.src = URL.createObjectURL(event.target.files[0]);
 
-
-  showImg.onload=function(){
+  showImg.onload = function () {
     URL.revokeObjectURL(showImg.src);
-  }
+  };
 }
 
-function pdfDown(){
-
-
+function pdfDown() {
   const doc = new jsPDF();
-
-  // doc.setFontSize(40);
-  // doc.text("www.abhirk.me", 35, 25);
   doc.addImage(newImg, 15, 40, 180, 180);
 
-  doc.save('imgintoPdf.pdf');
-   
-  
+  doc.save("imgintoPdf.pdf");
 }
